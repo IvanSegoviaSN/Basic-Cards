5278238a71276b14ca24299171637ec9622fc140
IvanSegoviaSN <ivansegsanz@gmail.com>
2021-02-18 00:23:55 +0100

    Finish proyect

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,10 +1,21 @@
+const BACKGROUND = {
+    card0: "https://fotos.subefotos.com/71c70cc8f6a9339b427886f76a0ef747o.jpg",
+    card1: "https://fotos.subefotos.com/dc1dc10756b3b43a4e912b0c99ed03b1o.jpg",
+    card2: "https://fotos.subefotos.com/b5bfb19ca46a3e61eab8b0737de3edcao.jpg",
+    card3: "https://fotos.subefotos.com/aaa48c9486e6222112c186ec59a05777o.jpg",
+    card4: "https://fotos.subefotos.com/94b36f3996551facab1ac321dd9dc2e5o.jpg",
+    card5: "https://fotos.subefotos.com/18765251021df55544524594cfc37b02o.jpg",
+    card6: "https://fotos.subefotos.com/89163520229d4fe077802284c3ebbc82o.jpg",
+    card7: "https://fotos.subefotos.com/cad0575bf0d44281f128a34ce4c21693o.jpg"
+}
+
 let cardsList = [
-    new Card("Prueba1", "Esto es una prueba de una prueba", "Fuerza", 'https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/143982823_741236623180838_7233973581989744866_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=KsDKCnmb_hsAX9D41HE&tp=1&oh=960f2ea5694a78bfb1c02921647b4cb8&oe=605530A6'),
-    new Card("Prueba2", "Esto es una prueba de una prueba", "Volador", 'url_background'),
-    new Card("Prueba3", "Esto es una prueba de una prueba", "Magia", 'url_background'),
-    new Card("Prueba4", "Esto es una prueba de una prueba", "Volador", 'url_background'),
-    new Card("Prueba5", "Esto es una prueba de una prueba", "Fuerza", 'url_background'),
-    new Card("Prueba6", "Esto es una prueba de una prueba", "Fuerza", 'url_background'),
-    new Card("Prueba7", "Esto es una prueba de una prueba", "Magia", 'url_background'),
-    new Card("Prueba8", "Esto es una prueba de una prueba", "Fuerza", 'url_background'),
+    new Card("Bob esponja", "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.", "Dibujos", BACKGROUND.card0),
+    new Card("Calamardo", "El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras.", "Dibujos", BACKGROUND.card1),
+    new Card("Bugs Bunny", "Hay muchas variaciones de los pasajes de Lorem Ipsum disponibles.", "Dibujos", BACKGROUND.card2),
+    new Card("Platanos", "Esto es una prueba de una prueba.", "Ficticio", BACKGROUND.card3),
+    new Card("Teletubbie", "Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.", "Dibujos", BACKGROUND.card4),
+    new Card("Pepa Pig", "Todos los generadores de Lorem Ipsum que se encuentran en Internet tienden a repetir trozos predefinidos cuando sea necesario.", "Dibujos", BACKGROUND.card5),
+    new Card("Payaso", "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio.", "Ficticio", BACKGROUND.card6),
+    new Card("Cerdo", "No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos.", "Barcenas", BACKGROUND.card7),
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,20 @@ function closeCardFooter(cardItem) {
     cardItem.getElementsByClassName('card-footer')[0].style.display = 'none';
 }
 
-function showModal() {
+function showModal(cardItem) {
+    loadModalValues(cardItem);
     document.getElementById("mainModal").style.display = "block";
 
     body.style.position = "static";
     body.style.height = "100%";
     body.style.overflow = "hidden";
+}
+
+function resetFormModal() {
+    document.getElementById('background').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('category').value = '';
 }
 
 function closeModal(saveForm = false) {
@@ -40,42 +48,52 @@ function closeModal(saveForm = false) {
     cardTemp = "";
 }
 
-function resetFormModal() {
-    document.getElementById('title').value = '';
-    document.getElementById('description').value = '';
-    document.getElementById('category').value = '';
-}
-
 function printModalValues(cardItem, idCard) {
+    cardItem.getElementsByClassName('card-body cardImg')[0].style.backgroundImage = cardsList[idCard].background;
     cardItem.getElementsByClassName('card-title')[0].innerText = cardsList[idCard].title;
     cardItem.getElementsByClassName('card-text')[0].innerText = cardsList[idCard].description;
     cardItem.getElementsByClassName('card-link')[0].innerText = cardsList[idCard].category;
+
+    reloadMain();
 }
 
 function saveModalValues(cardItem) {
     let indexToCardsList = cardItem.id.substr(5, cardItem.id.length);
 
+    cardsList[indexToCardsList].background = document.getElementById('background').value;
     cardsList[indexToCardsList].title = document.getElementById('title').value;
     cardsList[indexToCardsList].description = document.getElementById('description').value;
-
-    let category = document.getElementById('category').value;
-    cardsList[indexToCardsList].category = category == 0 ? 'Magia' : category == 1 ? 'Fuerza' : 'Volador';
+    cardsList[indexToCardsList].category = document.getElementById('category').value;
 
     printModalValues(cardItem, indexToCardsList);
 }
 
+function loadModalValues(cardItem) {
+    let indexToCardsList = cardItem.id.substr(5, cardItem.id.length);
+
+    document.getElementById('background').value = cardsList[indexToCardsList].background;
+    document.getElementById('title').value = cardsList[indexToCardsList].title;
+    document.getElementById('description').value = cardsList[indexToCardsList].description;
+    document.getElementById('category').value = cardsList[indexToCardsList].category;
+}
 
 /*
     Main
  */
 
+function reloadMain(element = document.getElementById('cardBox')) {
+    while (element.firstChild)
+        element.removeChild(element.firstChild);
+
+    createMain();
+}
 
 function createMain() {
-
     for (let i = 0; i < cardsList.length; i++) {
         document.getElementById('cardBox').innerHTML += '' +
+
             '              <div class="col-3 card cardItem" id="card-' + i + '" name="cardItem" onmouseover="openCardFooter(this)" onmouseout="closeCardFooter(this)">' +
-            '                  <div class="card-body cardImg" style="background-image: url(' + cardsList[i].background + ')"></div>' +
+            '                  <div class="card-body cardImg" id="backgroundImage" style="background-image: url(' + cardsList[i].background + ')"></div>' +
             '                  <div class="card-footer" style="display: none">' +
             '                       <h5 class="card-title">' + cardsList[i].title + '</h5>' +
             '                       <p class="card-text">' + cardsList[i].description + '</p>' +
@@ -87,12 +105,21 @@ function createMain() {
     // Añade el evento y envía la carta
     document.getElementsByName('cardItem').forEach(function (cardItem) {
         cardItem.addEventListener('click', function () {
-            showModal();
+            showModal(cardItem);
             cardTemp = cardItem;
         });
     });
 
+    // Ordena el array y vuelve a imprimir los datos
+    document.getElementById('orderButtonCards').addEventListener('click', function () {
+        cardsList.sort(function (a, b) {
+            if (a.category > b.category) {
+                return 1;
+            } else { return 0; }
+        });
 
+        reloadMain();
+    });
 }
 
 
